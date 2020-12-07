65c02995a59be5a3c159547b80587e60d7226634
seyoung5744 <seyoung574458@gmail.com>
2020-12-07 15:14:41 +0900

    JDBC and DAO

---
+++ java work/jdbc_exam/src/jdbc_exam/DBUtil.java
@@ -0,0 +1,44 @@
+package jdbc_exam;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class DBUtil {
+	static String URL = "jdbc:mysql://localhost:3306/testdb?serverTimezone=UTC"; // 보통 약속된 형식이 있음. 각 DBMS마다 조금씩 차이는 있지만
+	static String USER = "testuser";
+	static String PASSWORD = "won9975744!";
+	static String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+
+	// 접속 - Connection객체를 얻어온다
+	public static Connection getConnection() throws Exception {
+		Connection conn = null;
+		// 1. 클래스 로딩
+		Class.forName(DRIVER_NAME);
+		// 2. 드라이브 매니저로 connection 객체 얻어오기
+		conn = DriverManager.getConnection(URL, USER, PASSWORD);
+
+		return conn;
+	}
+
+	// 접속 종료
+	public static void close(Connection conn) {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	public static void close(Connection conn, PreparedStatement ps) {
+		if (ps != null) {
+			try {
+				ps.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		close(conn);
+	}
+}
--- java work/jdbc_exam/src/jdbc_exam/DeptDAO.java
+++ java work/jdbc_exam/src/jdbc_exam/DeptDAO.java
@@ -1,5 +1,161 @@
 package jdbc_exam;
 
-public class DeptDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+// DAO의 단위는 테이블당 하나
+public class DeptDAO {
+	// 이렇게 연결, statement 객체를 전역으로 선언해두면 안됨. 즉, 통로를 하나 만들어두고 그걸 공용해서 사용하는 것
+	// connect은 각각의 작업마다 만들어둬야 함.
+	//
+	// 좋은 방법은 연결된 풀을 만들어 두고 이용하는것. 즉, 중간 단계를 두고 사용.
+	// => 일 하나 할때마다 DB에 connect을 하면 드는 비용이 많기 때문에 미리 연결해두고 거기에서 할당받아 사용.
+
+	// 지금 코드에선 DB에 연결 종료하는 코드가 중복되서 불편하다...그래서 DBUtil이라고 따고 분리.
+	
+	private Connection conn = null;
+	private PreparedStatement ps = null;
+//
+//	public void insertDept(int deptno, String dname, String loc) {
+//		// 1. DB접속 Connection 객체를 얻어온다.
+//		String url = "jdbc:mysql://localhost:3306/testdb?serverTimezone=UTC"; // 보통 약속된 형식이 있음. 각 DBMS마다 조금씩 차이는 있지만
+//		String user = "testuser";
+//		String password = "won9975744!";
+//
+//		try {
+//			Class.forName("com.mysql.cj.jdbc.Driver");
+//			conn = DriverManager.getConnection(url, user, password);
+//
+//		// 2. 쿼리 작성
+//			String sql = "insert into dept(deptno, dname, loc) values (?,?,?)";
+//			ps = conn.prepareStatement(sql);
+//			
+//			// 값에 대한 바인딩
+//			ps.setInt(1, deptno);
+//			ps.setString(2, dname);
+//			ps.setString(3, loc); // query 까지 만들어두고 실행은 안한 상태
+//
+//		// 3. 쿼리 실행
+//			count = ps.executeUpdate();
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} finally {
+//		// 4. 접속 종료!!
+//			if (ps != null) {
+//				try {
+//					ps.close();
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//			if(conn != null) {
+//				try {
+//					conn.close();					
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		}
+//	}
+//
+//	public int updateDept(int deptno, String dname, String loc) {
+//		int count = 0;
+//		// 1. DB접속 Connection 객체를 얻어온다.
+//		String url = "jdbc:mysql://localhost:3306/testdb?serverTimezone=UTC"; // 보통 약속된 형식이 있음. 각 DBMS마다 조금씩 차이는 있지만
+//		String user = "testuser";
+//		String password = "won9975744!";
+//
+//		try {
+//			Class.forName("com.mysql.cj.jdbc.Driver");
+//			conn = DriverManager.getConnection(url, user, password);
+//
+//		// 2. 쿼리 작성
+//			String sql = "update dept set dname = ? where deptno = ?";
+//			ps = conn.prepareStatement(sql);
+//			// 값에 대한 바인딩
+//			ps.setString(1, dname); // 컬럼명 or ?순서 = 1
+//			ps.setInt(2, deptno);
+//
+//		// 3. 쿼리 실행
+//			count = ps.executeUpdate();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		} finally {
+//		// 4. 접속 종료!!
+//			if (ps != null) {
+//				try {
+//					ps.close();
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//			if(conn != null) {
+//				try {
+//					conn.close();					
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		}
+//		return count;
+//	}
+	
+	/*
+	 * DBUtil 만들고 재수정한 코드
+	 */
+	
+	public void insertDept(int deptno, String dname, String loc) {
+		PreparedStatement ps = null;
+		try {
+		// 1. DB접속 Connection 객체를 얻어온다.
+			conn = DBUtil.getConnection();
+
+		// 2. 쿼리 작성
+			String sql = "insert into dept(deptno, dname, loc) values (?,?,?)";
+			ps = conn.prepareStatement(sql);
+			
+			// 값에 대한 바인딩
+			ps.setInt(1, deptno);
+			ps.setString(2, dname);
+			ps.setString(3, loc); // query 까지 만들어두고 실행은 안한 상태
+
+		// 3. 쿼리 실행
+			ps.executeUpdate();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+		// 4. 접속 종료!!
+			DBUtil.close(conn, ps);
+		}
+	}
+
+	public int updateDept(int deptno, String dname, String loc) {
+		PreparedStatement ps = null;
+		int count = 0;
+		try {
+		// 1. DB접속 Connection 객체를 얻어온다.
+			conn = DBUtil.getConnection();
+		// 2. 쿼리 작성
+			String sql = "update dept set dname = ? loc = ?where deptno = ?";
+			ps = conn.prepareStatement(sql);
+			// 값에 대한 바인딩
+			ps.setString(1, dname); // 컬럼명 or ?순서 = 1
+			ps.setString(2, loc);
+			ps.setInt(3, deptno);
+
+		// 3. 쿼리 실행
+			count = ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+		// 4. 접속 종료!!
+			DBUtil.close(conn, ps);
+		}
+		
+		return count;
+	}
 }
